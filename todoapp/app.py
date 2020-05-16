123167a53157d27fb5c061c6c876628e279c3b60
Dulguun Battulga <dolugen@users.noreply.github.com>
2020-05-16 16:13:29 +0200

    async request

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dulguun:dulguun@localhost:5432/todoapp'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
   __tablename__ = 'todos'
@@ -18,7 +21,24 @@ def index():
 
 @app.route('/new', methods=['POST'])
 def add_new():
-    description = request.form.get('description')
-    db.session.add(Todo(description=description))
-    db.session.commit()
-    return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        description = request.get_json().get('description')
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body = {
+            'description': todo.description
+        }
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
