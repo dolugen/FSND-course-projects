5d1548414e9cb5304a3e3787ea774fd2f8856978
Dulguun Battulga <dolugen@users.noreply.github.com>
2020-05-16 15:27:55 +0200

    flask demo

--- flask-hello/app.py
+++ flask-hello/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dulguun:dulguun@localhost:5432/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
@@ -10,8 +11,12 @@ class Person(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        return f'<Person: {self.id} {self.name}>'
+
 db.create_all()
 
 @app.route('/')
 def index():
-    return 'hello world!'
+    person = Person.query.first()
+    return 'hello ' + person.name
