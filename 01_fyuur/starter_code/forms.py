0405169325f0e247111ea2816eec32b2016106b5
Dulguun Battulga <dolugen@users.noreply.github.com>
2020-05-19 21:17:24 +0200

    prepare to wrapup

--- 01_fyuur/starter_code/forms.py
+++ 01_fyuur/starter_code/forms.py
@@ -121,7 +121,6 @@ class VenueForm(Form):
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
         choices=Genres.field_choices()
     )
